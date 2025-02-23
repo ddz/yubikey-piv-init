d2b71273b5da4796cbe5a05533a98a45e44a92d6
Dino Dai Zovi <3083278+ddz@users.noreply.github.com>
2025-02-23 12:58:06 -0500

    Simplify to only what is used by macOS sc_pair and ssh-keychain.dylib

--- yubikey-piv-init.sh
+++ yubikey-piv-init.sh
@@ -49,30 +49,24 @@ function main() {
     echo "Resetting PIV applet..."
     ykman piv reset
 
-    echo "Generating CCC and CHUID..."
-    ykman piv objects generate ccc -m ${KEY} -P ${PIN}
+    echo "Generating CHUID..."
     ykman piv objects generate chuid -m ${KEY} -P ${PIN}
 
     #
     # Generate PIV keys and use attestation certificates as their certs
     #
 
-    # Use RSA for compat w/ macOS SSH PKCS11 library, etc.
-    echo "Generating PIV Authentication certificate..."
-    generate_key 9a RSA2048 "--pin-policy ONCE --touch-policy CACHED"
-
-    # Digital signature algorithm, use the strongest available
-    echo "Generating Digital Signature certificate..."
-    generate_key 9c ECCP384 "--pin-policy ALWAYS --touch-policy ALWAYS"
-
     # macOS does not support P-384 for PIV-based logon, so use P-256
+    echo "Generating PIV Authentication certificate..."
+    generate_key 9a ECCP256 "--pin-policy ONCE --touch-policy ALWAYS"
+
     echo "Generating Key Management certificate..."
     generate_key 9d ECCP256 "--pin-policy ONCE --touch-policy CACHED"
 
     # Use RSA for Card Auth for compat w/ macOS SSH PKCS11 library to
     # be able to use this slot for device-based authentication
     echo "Generating Card Authentication certificate..."
-    generate_key 9e RSA2048 "--pin-policy NEVER --touch-policy CACHED"
+    generate_key 9e RSA2048 "--pin-policy NEVER --touch-policy ALWAYS"
 
     #
     # Reset management key, PIN, and PUK to random values
